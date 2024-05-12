3595eaabeab1b0e325351ef0f20e0eac6f93a7b2
Schukinsky <152327391+Schukinsky@users.noreply.github.com>
2024-05-12 20:33:38 +0300

    Update procedure_create_user_.sql

--- procedure_create_user_.sql
+++ procedure_create_user_.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE PROCEDURE gate_01.create_user(
+CREATE OR REPLACE PROCEDURE gate01.create_user(
     IN p_last_name TEXT,
     IN p_first_name TEXT,
     IN p_middle_name TEXT,
@@ -13,15 +13,15 @@ DECLARE
     v_user_id INT;
 BEGIN
     -- Проверяем, существует ли адрес в таблице Address
-    SELECT id INTO v_address_id FROM Address WHERE name = p_address_name;
+    SELECT id INTO v_address_id FROM address WHERE name = p_address_name;
 
     -- Если адрес не найден, создаем новый и получаем его идентификатор
     IF NOT FOUND THEN
-        INSERT INTO Address (name) VALUES (p_address_name) RETURNING id INTO v_address_id;
+        INSERT INTO address (name) VALUES (p_address_name) RETURNING id INTO v_address_id;
     END IF;
 
     -- Создаем запись в таблице User
-    INSERT INTO "User" (last_name, first_name, middle_name, phone, email, FK_address)
+    INSERT INTO "user" (last_name, first_name, middle_name, phone, email, FK_address)
     VALUES (p_last_name, p_first_name, p_middle_name, p_phone, p_email, v_address_id)
     RETURNING id INTO v_user_id;
 
@@ -31,4 +31,4 @@ END;
 $$;
 
 
-CALL gate_01.create_user('Doe', 'John', 'Robert', '123456789', 'john.doe@example.com', '123 Main St');
+CALL gate01.create_user('Doe', 'John', 'Robert', '123456789', 'john.doe@example.com', '123 Main St');
